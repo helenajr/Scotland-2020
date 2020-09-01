17baac7353a2eba3c6b16838c67533f320420b4d
helenajr <helenajrobinson@gmail.com>
2020-09-01 20:04:48 +0100

    Added comments, car ride and adjusted numbers of people

--- car_script.R
+++ car_script.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(dplyr)
 library(ggplot2)
 
-scotland <- read.csv("~/Desktop/Scotland-2020/Scotland 2020 - Sheet1.csv")
+scotland <- read.csv("~/Desktop/Scotland-2020/Scotland 2020 car ride - Sheet1.csv")
 
 scotland$Name <- as.factor(scotland$Name)
 
@@ -16,7 +16,7 @@ person_totals
 as.data.frame(person_totals)
 colnames (person_totals) <- c("name", "amount_spent")
 
-num_people <- 6
+num_people <- 3
 
 #Cost of the trip per person#
 expense_per_head <- sum(person_totals$amount_spent)/ num_people
